bbf6b185ad5c0495fd20a902648c876fb6de5388
resqiar <resqiagengr@gmail.com>
2024-04-01 02:15:17 +0700

    add basic migrations for users table

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+	provider   VARCHAR(20),
+	fullname   VARCHAR(100),
+	username   VARCHAR(100) UNIQUE NOT NULL,
+	email   VARCHAR(100) UNIQUE NOT NULL,
+	bio        TEXT,
+	picture_url TEXT
+);
