8ccfa39c71fbeb57b4188e0bd2090d474d937523
resqiar <resqiagengr@gmail.com>
2024-04-01 02:42:55 +0700

    add callback functions

--- routes/api_route.go
+++ routes/api_route.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"binder/services"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,7 +10,5 @@ func InitAPIRoutes(e *echo.Echo) {
 	auth := e.Group("auth")
 
 	auth.GET("/google", services.GoogleLoginService)
-	auth.GET("/google/callback", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Google Callback")
-	})
+	auth.GET("/google/callback", services.GoogleLoginCallbackService)
 }
--- services/auth_service.go
+++ services/auth_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"binder/configs"
+	"binder/utils"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -12,4 +13,25 @@ func GoogleLoginService(c echo.Context) error {
 	URL := conf.AuthCodeURL("not-implemented-yet")
 
 	return c.Redirect(http.StatusTemporaryRedirect, URL)
+}
+
+func GoogleLoginCallbackService(c echo.Context) error {
+	code := c.QueryParam("code")
+	if code == "" {
+		return c.NoContent(http.StatusUnauthorized)
+	}
+
+	// exchange token retrieved from google with valid token
+	token, err := configs.GoogleConfig.Exchange(c.Request().Context(), code)
+	if err != nil {
+		return c.NoContent(http.StatusUnauthorized)
+	}
+
+	// convert token to user profile data
+	payload, err := utils.ConvertGoogleToken(token.AccessToken)
+	if err != nil {
+		return c.NoContent(http.StatusUnauthorized)
+	}
+
+	return c.JSON(http.StatusOK, payload)
 }
