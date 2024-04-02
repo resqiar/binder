615247ff51a05694f02a4bb4eba0e4e332f366ac
resqiar <resqiagengr@gmail.com>
2024-04-02 23:47:17 +0700

    differentiate common navbar and create navbar

--- views/pages/CreateExtPage_templ.go
+++ views/pages/CreateExtPage_templ.go
@@ -38,7 +38,7 @@ func CreateExtPage() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = navbar.Navbar().Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = navbar.CreateNavbar().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
